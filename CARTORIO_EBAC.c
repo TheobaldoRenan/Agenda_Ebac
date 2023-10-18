10f17409001f07d2bc63e080e0358adb28eeb511
Renan Theobaldo <148164318+TheobaldoRenan@users.noreply.github.com>
2023-10-18 20:17:46 -0300

    Ajustes de codigo
    Identação,  erro de escrita por causa de assento  no menu de enha

--- CARTORIO_EBAC.c
+++ CARTORIO_EBAC.c
@@ -4,7 +4,7 @@
 #include <string.h>		 //biblioteca de administração de strings
 
 int
- registro()																	// Função de Registrar Nomes
+ registro()																		// Função de Registrar Nomes
 {
 	char arquivo[40];														//inicio de criação de variaveis tipo string
 	char cpf[40];
@@ -12,7 +12,7 @@ int
 	char sobrenome[40];
 	char cargo[40];															//final de criação de variaveis tipo string
 	
-	printf("Digite o CPF a ser cadastrado: \n");						// coletar informação do usuario 
+	printf("Digite o CPF a ser cadastrado: \n");							// coletar informação do usuario 
 	printf("CPF: ");	
 	scanf("%s", cpf);														//%s refere-se a strings
 	
@@ -52,17 +52,14 @@ int
 	fclose(file);
 	
 	printf("\n Digite o CARGO a ser cadastrado:\n");
-	printf("Cargo: ")
-;	scanf("%s", cargo);
+	printf("Cargo: ");
+	scanf("%s", cargo);
 	
 	file = fopen(arquivo,"a");
 	fprintf(file,cargo);
 	fclose(file);
 	
-	system("pause");
-	
-	
-	
+	system("pause");	
 }
 
 
@@ -73,15 +70,16 @@ int consulta()																	//função de Consultar Nomes
 	char cpf[40];
 	char conteudo[200];
 	
-	printf("Digite o CPF a ser consultado: \n");
+	printf("\n\n Digite o CPF a ser consultado:");								//recebendo o usuario a ser consultado
 	scanf("%s", cpf);
 	
 	FILE *file;
-	file = fopen(cpf,"r");
+	file = fopen(cpf,"r");														//lendo o arquivo
 	
 	if(file == NULL)															//verificando se o CPF existe
 	{
-		printf("CPF não ENCONTRADO !!! \n");	
+		printf("\n\n");
+		printf("CPF NÃO ENCONTRADO !!! \n\n\n");	
 	}
 
 	while(fgets(conteudo,200,file) != NULL)										//buscando ate 200 caracteres dentro do arquivo até for nulo
@@ -107,9 +105,9 @@ int deletar()																	//função de Deletar Nomes
 	fclose(file);	
 	
 	
-	if (file == NULL)
+	if (file == NULL)															// Se o cpf nao tiver cadastrado exibe mensagem
 	{
-		printf("\t Usuario não encontrado !! \n\n");
+		printf("\t\n\n Usuario não encontrado !! \n\n");
 		system("pause");
 	}
 
@@ -118,73 +116,84 @@ int deletar()																	//função de Deletar Nomes
 	
 	if (file != NULL)
 	{
-		printf("\t Usuario Deletado com Sucesso ! \n\n\n");
+		printf("\t\n\n Usuario Deletado com Sucesso ! \n\n\n");						// caso encontrado mensagem de confirmação de exclusão
 		system("pause");
 	}
-	
-	
 }
 
 
 
-int main ()
+int main ()																		//Função Principal - Tela de MENU
 {
-	int opcao=0;																//Definindo as variaveis  do menu
+	int opcao=0;																	//Definindo as variaveis  do menu
 	int loop=1;
+	char getpassword[10]="a";
+	int comparacao;
 	
-	for(loop=1;loop=1;)															//criando loop do programa
-	{	
-		
-		system("cls");
-		
-		setlocale(LC_ALL, "Portuguese");											//Setando a lingua de saida para PORTUGUES
+	setlocale(LC_ALL, "Portuguese");
 	
-		printf("\t\t\t\t_____  Cartório da EBAC  _____ \n\n");								//Inicio Menu
-		printf("Escolha a opção desejada:\n\n");
-		printf("\t 1 - Registrar Nomes.\n\n");
-		printf("\t 2 - Consultar Nomes.\n\n");
-		printf("\t 3 - Excluir Nomes.\n\n");
-		printf("\t 4 - SAIR \n\n ");										
-		printf("Opção:");															//Final Menu
-		scanf("%d", &opcao);														// Escolha do usuario
+	printf("\t\t\t\t_____  Cartório da EBAC  _____ \n\n");
+	printf("LOGIN DE ADMIN !! \n\n Digite a senha: ");
+	scanf("%s",getpassword);
+	
+	comparacao = strcmp(getpassword,"Admin");										//comparando 2 strings. verificando se a senha esta correta
+	
+	if (comparacao == 0)
+	{
+		
+		for(loop=1;loop=1;)																//criando loop do programa
+		{	
+		
+			system("cls");
+		
+			setlocale(LC_ALL, "Portuguese");											//Setando a lingua de saida para PORTUGUES
+	
+			printf("\t\t\t\t_____  Cartório da EBAC  _____ \n\n");						//Inicio Menu
+			printf("Escolha a opção desejada:\n\n");
+			printf("\t 1 - Registrar Nomes.\n\n");
+			printf("\t 2 - Consultar Nomes.\n\n");
+			printf("\t 3 - Excluir Nomes.\n\n");
+			printf("\t 4 - SAIR \n\n ");										
+			printf("Opção:");															//Final Menu
+			scanf("%d", &opcao);														// Escolha do usuario
 		
 	
 		
 		
-		system("cls");																// limpando a tela após escolha do menu
+			system("cls");																// limpando a tela após escolha do menu
 	
-			switch(opcao)															// inicio da seleção do menu
-			{
-			case 1:
-				registro();															//chamada de função
-			break;																	//final da função caso seja 1
+				switch(opcao)															// inicio da seleção do menu
+				{
+				case 1:
+					registro();															//chamada de função
+				break;																	//final da função caso seja 1
 			
-			case 2:
-				consulta();															//chamada de função
-			break;																	//final da função caso seja 2
+				case 2:
+					consulta();															//chamada de função
+				break;																	//final da função caso seja 2
 			
-			case 3:
-				deletar();															//chamada de função
-			break;																	// final da função caso seja 3
+				case 3:
+					deletar();															//chamada de função
+				break;																	// final da função caso seja 3
 			
-			case 4:
-				printf(" \t\t OBRIGADO POR UTILIZAR O SISTEMA. \n\n\n\n");	
-				return 0;
-			break;
+				case 4:
+					printf(" \t\t OBRIGADO POR UTILIZAR O SISTEMA. \n\n\n\n");			//mensagem de saida do programa
+					return 0;															//saindo do programa
+				break;
 				
 										
-			default:																//reorna valor padrão caso nenhuma opçao acima seja efetivada
-				printf(" !!!    Opção invalida    !!!  \n\n\n");					//comunicando falha na escolha de opção
-				system("pause");
-			break;
-				
-		}
-		
+				default:																//reorna valor padrão caso nenhuma opçao acima seja efetivada
+					printf(" !!!    Opção invalida    !!!  \n\n\n");					//comunicando falha na escolha de opção
+					system("pause");
+				break;
 	
+				}
+			}	
 	}
-	
-	
-	
+		
+		else
+			system("cls");
+			printf("\t\t\n\n!!!! SENHA INCORRETA !!!!\n\n\n\n");
 	
 }
 
